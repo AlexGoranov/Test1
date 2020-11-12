190765c21ab56ec2bdc9ab2cd07112eac18b81ba
Alex Goranov <a.goranov@ampada.de>
2020-11-12 17:13:56 +0100

    Assignment 2 was completed

---
+++ src/app/components/even-numbers/even-numbers.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-even-numbers',
+  templateUrl: './even-numbers.component.html',
+  styleUrls: ['./even-numbers.component.css']
+})
+export class EvenNumbersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
