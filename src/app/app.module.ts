a8f665e0b920407accfbe7f2b3ffa630cb076d31
Alex Goranov <a.goranov@ampada.de>
2020-11-12 17:21:14 +0100

    Created an instructor component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { OddNumbersComponent } from './components/odd-numbers/odd-numbers.component';
 import { EvenNumbersComponent } from './components/even-numbers/even-numbers.component';
+import { InstructorComponent } from './components/instructor/instructor.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     OddNumbersComponent,
-    EvenNumbersComponent
+    EvenNumbersComponent,
+    InstructorComponent
   ],
   imports: [
     BrowserModule
