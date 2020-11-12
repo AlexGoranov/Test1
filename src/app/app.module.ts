b7d60f2d0afca0a6048448654dafdeddb6ee817c
Alex Goranov <a.goranov@ampada.de>
2020-11-12 17:11:29 +0100

    Assignment 4 completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { OddNumbersComponent } from './components/odd-numbers/odd-numbers.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OddNumbersComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/odd-numbers/odd-numbers.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-odd-numbers',
+  templateUrl: './odd-numbers.component.html',
+  styleUrls: ['./odd-numbers.component.css']
+})
+export class OddNumbersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
